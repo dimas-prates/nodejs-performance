2859b6a33f86e70ce48bcb1c0d4f883188e256aa
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-08-27 22:27:06 -0300

    :boom: fix(server.ts): correct typo and add styles(comma)

--- src/server.ts
+++ src/server.ts
@@ -5,25 +5,24 @@ const appPort = process.env.PORT || 3000
 const processId = process.pid
 
 app.get("/", (req, res) => {
-    //forcing crash
-    for (let index = 0; index < 1e7; index++)
+    for (let index = 0; index < 1e7; index++);
     res.status(200).json({ message: `Handdled by ${processId}` })
-})
+});
 
 const server = app.listen(appPort, () => {
     console.log(`Server started (PID: ${processId}) on PORT: ${appPort}\n${new Date()}`)
-})
+});
 
 process.on('SIGINT', (signal) => {
     console.log(signal)
     server.close(() => { console.log(`PID:${processId} terminated`); process.exit(0) })
     setTimeout(() => { process.exit(0) }, 1000).unref()
-})
+});
 process.on('SIGTERM', (signal) => {
     console.log(signal)
     server.close(() => { console.log(`PID:${processId} terminated`); process.exit(0) })
     setTimeout(() => { process.exit(0) }, 1000).unref()
-})
+});
 
 process.on("uncaughtException", (error, origin) => {
     server.close(() => { console.log(`${origin}: ${error}\nPID:${processId} terminated`); process.exit(1) })
@@ -33,4 +32,4 @@ process.on("unhandledRejection", (reason) => {
 });
 
 //forcing abruptly termination
-setTimeout(() => { process.exit(1) }, Math.random() * 1e4)
+// setTimeout(() => { throw new Error('FORCED ERROR\n') }, Math.random() * 1e4)
